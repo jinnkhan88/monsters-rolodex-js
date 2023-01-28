9686ac68e4f2daf75628ab00e25e09498f4ae521
Asad Rehman <asad.rehman>
2023-01-28 15:12:05 +0500

    final commit for deployment

---
+++ src/components/card-list/card-list.component.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Card from "../card/card.component";
+import "./card-list.styles.css";
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return <Card key={monster.email} monster={monster} />;
+      })}
+    </div>
+  );
+};
+
+export default CardList;
---
+++ src/components/search-box/search-box.component.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import "./search-box.styles.css";
+
+const SearchBox = ({ onChangeHandler, placeholder, className }) => {
+  return (
+    <input
+      className={`search-box ${className}`}
+      type="search"
+      placeholder={placeholder}
+      onChange={onChangeHandler}
+    />
+  );
+};
+export default SearchBox;
